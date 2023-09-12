113caffe19fee3e4fe81260dd9d78da0f0b4ca1f
Muhammad Sulaiman Hosain <solaiman.sh6@gmail.com>
2023-09-12 08:23:45 +0600

    Updated Login Page

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -39,7 +39,49 @@
         <div class="container d-flex flex-column">
             <div class="row vh-100">
                 <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
-                    <div class="d-table-cell align-middle"></div>
+                    <div class="d-table-cell align-middle">
+
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="m-sm-4">
+                                    <div class="text-center">
+                                        <img src="{{ asset('assets/backend') }}/img/avatars/avatar.jpg" alt="Charles Hall"
+                                            class="img-fluid rounded-circle" width="132" height="132" />
+                                    </div>
+                                    <form method="POST" action="{{ route('backend.admin.login') }}">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label class="form-label">Email</label>
+                                            <input class="form-control form-control-lg" type="email" name="email"
+                                                placeholder="Enter your email" />
+                                        </div>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                        <div class="mb-3">
+                                            <label class="form-label">Password</label>
+                                            <input class="form-control form-control-lg" type="password" name="password"
+                                                placeholder="Enter your password" />
+                                            <small>
+                                                <a href="pages-reset-password.html">Forgot password?</a>
+                                            </small>
+                                        </div>
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+
+                                        <div class="text-center mt-3">
+                                            <button type="submit" class="btn btn-lg btn-primary">Sign in</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                     {{-- <div class="text-center mt-4">
                         <h1 class="h2">Welcome back, Charles</h1>
@@ -48,46 +90,7 @@
                         </p>
                     </div> --}}
 
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="m-sm-4">
-                                <div class="text-center">
-                                    <img src="{{ asset('assets/backend') }}/img/avatars/avatar.jpg" alt="Charles Hall"
-                                        class="img-fluid rounded-circle" width="132" height="132" />
-                                </div>
-                                <form method="POST" action="{{ route('backend.admin.login') }}">
-                                    @csrf
-                                    <div class="mb-3">
-                                        <label class="form-label">Email</label>
-                                        <input class="form-control form-control-lg" type="email" name="email"
-                                            placeholder="Enter your email" />
-                                    </div>
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                    <div class="mb-3">
-                                        <label class="form-label">Password</label>
-                                        <input class="form-control form-control-lg" type="password" name="password"
-                                            placeholder="Enter your password" />
-                                        <small>
-                                            <a href="pages-reset-password.html">Forgot password?</a>
-                                        </small>
-                                    </div>
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
 
-                                    <div class="text-center mt-3">
-                                        <button type="submit" class="btn btn-lg btn-primary">Sign in</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
 
                 </div>
             </div>
