2724757783b7a5c490d1c894f17c1acdf4513a3d
Muhammad Sulaiman Hosain <solaiman.sh6@gmail.com>
2023-09-16 11:08:23 +0600

    Report Page updated

---
+++ resources/views/backend/report/rentReport.blade.php
@@ -0,0 +1,43 @@
+@extends('backend.layout.app')
+@push('css')
+@endpush
+@section('content')
+@section('page-header')
+<i class="fa fa-plus-circle"></i>  Rental Collection Report Form
+@stop
+@section('table_header')
+@include('backend._partials.page_header', [
+'fa' => 'fa fa-list',
+'name' => 'Reports',
+'route' =>route('backend.floor.index'),
+])
+@endsection
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            @yield('table_header')
+            <div class="card-body">
+                <form action="{{ route('backend.floor.store') }}" method="POST" class="row g-3">
+                    @method('POST')
+                    @csrf
+                    <div class="col-md-12">
+                        @include('components.backend.forms.input.input-type', [
+                        'name' => 'name',
+                        'required' => true,
+                        ])
+                        @include('components.backend.forms.input.errorMessage', ['message'=>$errors->first('name')])
+                    </div>
+
+                    <div class="col-12 text-center">
+                        <button class="btn btn-primary" type="submit">Submit Data</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('js')
+
+@endpush
