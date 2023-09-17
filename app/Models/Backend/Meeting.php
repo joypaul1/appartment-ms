ef506ace62a1a69c0b5e24f84d2eb9bdcf81c1f1
IcicleCorporation <Dev@iciclecorporation.com>
2023-09-17 21:56:27 +0600

    gg

--- app/Models/Backend/Meeting.php
+++ app/Models/Backend/Meeting.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meeting extends Model
 {
-    protected $table ='meetings';
+    protected $table ='management_committees';
 
     use GlobalScope, AutoTimeStamp;
     protected $guarded =['id'];
