49a7f94e4fd1aedbf662617ef3bc9240041a91d3
joypaul1 <jyla5102555@gmail.com>
2023-10-02 14:27:58 +0600

    dd

--- resources/views/backend/complain/index.blade.php
+++ resources/views/backend/complain/index.blade.php
@@ -1,17 +1,19 @@
 @extends('backend.layout.app')
+@include('backend._partials.delete_alert')
+
 @push('css')
 @endpush
 @section('content')
 
 @section('page-header')
-    <i class="fa fa-list"></i> {{ __('title.Complain-List') }}
+<i class="fa fa-list"></i> {{ __('title.Complain-List') }}
 @stop
 @section('table_header')
-    @include('backend._partials.page_header', [
-        'fa' => 'fa fa-plus-circle',
-        'name' => __('title.Create-Complain'),
-        'route' => route('backend.complain.create'),
-    ])
+@include('backend._partials.page_header', [
+'fa' => 'fa fa-plus-circle',
+'name' => __('title.Create-Complain'),
+'route' => route('backend.complain.create'),
+])
 @endsection
 
 
@@ -23,7 +25,7 @@
                 <table id="datatables-reponsive" class="table table-striped" style="width:100%">
                     <thead>
                         <tr>
-                            th>@lang('table.sl')</th>
+                            <th>@lang('table.sl')</th>
                             <th>@lang('table.title')</th>
                             <th>@lang('table.date')</th>
                             <th>@lang('table.action')</th>
@@ -31,46 +33,30 @@
                     </thead>
                     <tbody>
                         @foreach ($complains as $key => $row)
-                            <tr>
-                                <td>
-                                    {{ $key + 1 }}
-                                </td>
-                                <td>
-                                    {{ $row->title }}
-                                </td>
-                                <td>
-                                    {{ date('d-m-y', strtotime($row->date)) }}
-                                </td>
-                                {{-- <td>
-                                    {{ $row->status }}
-                                </td> --}}
-                                {{-- <td>
-                            {{ $row->status }}
-                        </td> --}}
+                        <tr>
+                            <td>
+                                {{ $key + 1 }}
+                            </td>
+                            <td>
+                                {{ $row->title }}
+                            </td>
+                            <td>
+                                {{ date('d-m-y', strtotime($row->date)) }}
+                            </td>
 
 
-                                <td class="table-action">
-                                    <a href="{{ route('backend.complain.edit', $row) }}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-edit-2 align-middle">
-                                            <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
-                                        </svg>
-                                    </a>
-                                    <a href="#">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-trash align-middle">
-                                            <polyline points="3 6 5 6 21 6"></polyline>
-                                            <path
-                                                d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                            </path>
-                                        </svg>
-                                    </a>
-                                </td>
-                            </tr>
+
+                            <td class="table-action">
+                                <a href="{{ route('backend.complain.edit', $row) }}">
+                                    <button class="btn btn-sm btn-info"> <i class="fa fa-pencil" aria-hidden="true"></i> </button>
+
+                                </a>
+                                <a data-href="{{ route('backend.complain.destroy', $row) }}" href="#" class="delete_check">
+                                    <button class="btn btn-sm btn-danger"> <i class="fa fa-trash" aria-hidden="true"></i> </button>
+
+                                </a>
+                            </td>
+                        </tr>
                         @endforeach
 
 
@@ -83,13 +69,7 @@
 @endsection
 
 @push('js')
-<script>
-    $(document).ready(function() {
-        $("#toggleFilter").click(function() {
-            $("#filterContainer").slideToggle();
-        });
-    });
-</script>
+
 
 <script>
     document.addEventListener("DOMContentLoaded", function() {
