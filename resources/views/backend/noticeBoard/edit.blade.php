3970756483b33e2eff38c95f505d94c2c463849e
joypaul1 <jyla5102555@gmail.com>
2023-10-09 21:17:40 +0600

    done

--- resources/views/backend/noticeBoard/edit.blade.php
+++ resources/views/backend/noticeBoard/edit.blade.php
@@ -3,7 +3,7 @@
 @endpush
 @section('content')
 @section('page-header')
-    <i class="fa fa-plus-circle"></i>{{ __('title.Edit-Notice-Board') }}
+    <i class="fa fa-plus-circle"></i> {{ __('title.Edit-Notice-Board') }}
 @stop
 @section('table_header')
     @include('backend._partials.page_header', [
