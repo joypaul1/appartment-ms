160cea23e4104796d3c3f23b9599b3f8280db3f3
joypaul1 <jyla5102555@gmail.com>
2023-10-10 04:00:58 +0600

    dd

--- resources/views/backend/report/incomeReportPdf.blade.php
+++ resources/views/backend/report/incomeReportPdf.blade.php
@@ -19,16 +19,17 @@ $remainBalance = $incomeTotal- $expenseTotal ;
         <div class="card p-3">
             <p class="text-center" id="print"><button class="btn btn-info"><i class="fa fa-print" aria-hidden="true"></i></button></p>
             <div id="printArea">
-                <div class="card mx-auto" style="width: 25%;">
+                <div class="card mx-auto  " style="width: 40%;">
                     <img class="card-img-top" src="{{ asset(session('site_info')['logo']) }}" alt="logo">
-                    <div class="card-body">
+                    <div class="card-body  text-center">
                         <strong class="card-header"><u>{{ $branch->name }}</u></strong>
                         <p class="card-text">Mobile : {{ $branch->mobile }}
                             <br> Email: {{ $branch->email }}
                         </p>
+                        <div class="card-title text-center"><u>Profit Report</u> </div>
+
                     </div>
                 </div>
-                <div class="card-title text-center"><u>Profit Report</u> </div>
                 <div class="card-body">
                     <div class="card-body">
                         <table id="datatables-reponsive" class="table table-bordered table-responsive  text-center" style="width:100%">
@@ -111,15 +112,4 @@ $remainBalance = $incomeTotal- $expenseTotal ;
         });
 </script>
 
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-            // Choices.js
-            new Choices(document.querySelector(".choices-single"));
-
-            // Responsive Datatables
-            $("#datatables-reponsive").DataTable({
-                responsive: true
-            });
-        });
-</script>
 @endpush
