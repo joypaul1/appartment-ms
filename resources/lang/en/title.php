cd74b251d7efc8ddcfb587a1acdb05b0cfb2d5e7
joypaul1 <jyla5102555@gmail.com>
2023-10-19 17:58:07 +0600

    dddd

--- resources/lang/en/title.php
+++ resources/lang/en/title.php
@@ -1,88 +1,86 @@
 <?php
 
 return [
-    'Floor-List' => 'Floor List',
-    'Create-Floor' => 'Create Floor',
-    'Edit-Floor' => 'Edit Floor',
+    'Floor-List'                   => 'Floor List',
+    'Create-Floor'                 => 'Create Floor',
+    'Edit-Floor'                   => 'Edit Floor',
 
-    'Fund-List' => 'Fund List',
-    'Create-Fund' => 'Create Fund',
-    'Edit-Fund' => 'Edit Fund',
+    'Fund-List'                    => 'Fund List',
+    'Create-Fund'                  => 'Create Fund',
+    'Edit-Fund'                    => 'Edit Fund',
 
-    'Unit-List' => 'Unit List',
-    'Create-Unit' => 'Create Floor',
-    'Edit-Unit' => 'Edit Unit',
+    'Unit-List'                    => 'Unit List',
+    'Create-Unit'                  => 'Create Floor',
+    'Edit-Unit'                    => 'Edit Unit',
 
-    'Admin-List' => 'Admin List',
-    'Create-Admin' => 'Create Admin',
-    'Edit-Admin' => 'Edit Admin',
+    'Admin-List'                   => 'Admin List',
+    'Create-Admin'                 => 'Create Admin',
+    'Edit-Admin'                   => 'Edit Admin',
 
-    'Bill-Type-List' => 'Bill Type List',
-    'Create-Bill-Type' => 'Create Bill Type',
-    'Edit-Bill-Type' => 'Edit Bill Type',
+    'Bill-Type-List'               => 'Bill Type List',
+    'Create-Bill-Type'             => 'Create Bill Type',
+    'Edit-Bill-Type'               => 'Edit Bill Type',
 
-    'Bill-Deposit-List' => 'Bill Deposit List',
-    'Create-Bill-Deposit' => 'Create Bill Deposit',
-    'Edit-Bill-Deposit' => 'Edit Bill Deposit',
-
-
-    'Complain-List' => 'Complain List',
-    'Create-Complain' => 'Create Complain',
-    'Edit-Complain' => 'Edit Complain',
-
-    'Notice-Board-List' => 'Notice-Board List',
-    'Create-Notice-Board' => 'Create Notice-Board',
-    'Edit-Notice-Board' => 'Edit Notice-Board',
-
-    'Employee-List' => 'Employee List',
-    'Create-Employee' => 'Create Employee',
-    'Edit-Employee' => 'Edit Employee',
-
-    'Employee-Salary-List' => 'Employee Salary List',
-    'Create-Employee-Salary' => 'Create Employee Salary',
-    'Edit-Employee-Salary' => 'Edit Employee Salary',
-
-    'Maintenance-Cost-List' => 'Maintenance Cost List',
-    'Create-Maintenance-Cost' => 'Create Maintenance Cost',
-    'Edit-Maintenance-Cost' => 'Edit Maintenance Cost',
+    'Bill-Deposit-List'            => 'Bill Deposit List',
+    'Create-Bill-Deposit'          => 'Create Bill Deposit',
+    'Edit-Bill-Deposit'            => 'Edit Bill Deposit',
 
 
-    'Management-Committe-List' => 'Management-Committe List',
-    'Create-Management-Committe' => 'Create Management-Committe',
-    'Edit-Management-Committe' => 'Edit Management-Committe',
+    'Complain-List'                => 'Complain List',
+    'Create-Complain'              => 'Create Complain',
+    'Edit-Complain'                => 'Edit Complain',
 
-    'Rent-Collection-List' => 'Rent-Collection-List',
-    'Create-Rent-Collection' => 'Create Rent-Collection',
-    'Edit-Rent-Collection' => 'Edit Rent-Collection',
+    'Notice-Board-List'            => 'Notice-Board List',
+    'Create-Notice-Board'          => 'Create Notice-Board',
+    'Edit-Notice-Board'            => 'Edit Notice-Board',
 
-    'Owner-List' => 'Owner List',
-    'Create-Owner' => 'Create Owner',
-    'Edit-Owner' => 'Edit Owner',
+    'Employee-List'                => 'Employee List',
+    'Create-Employee'              => 'Create Employee',
+    'Edit-Employee'                => 'Edit Employee',
+
+    'Employee-Salary-List'         => 'Employee Salary List',
+    'Create-Employee-Salary'       => 'Create Employee Salary',
+    'Edit-Employee-Salary'         => 'Edit Employee Salary',
+
+    'Maintenance-Cost-List'        => 'Maintenance Cost List',
+    'Create-Maintenance-Cost'      => 'Create Maintenance Cost',
+    'Edit-Maintenance-Cost'        => 'Edit Maintenance Cost',
 
 
+    'Management-Committe-List'     => 'Management-Committe List',
+    'Create-Management-Committe'   => 'Create Management-Committe',
+    'Edit-Management-Committe'     => 'Edit Management-Committe',
 
-    'Tenant-List' => 'Tenant List',
-    'Create-Tenant' => 'Create Tenant',
-    'Edit-Tenant' => 'Edit Tenant',
+    'Rent-Collection-List'         => 'Rent-Collection-List',
+    'Create-Rent-Collection'       => 'Create Rent-Collection',
+    'Edit-Rent-Collection'         => 'Edit Rent-Collection',
 
-    'Year-List' => 'Year List',
-    'Create-Year' => 'Create Year',
-    'Edit-Year' => 'Edit Tenant',
+    'Owner-List'                   => 'Owner List',
+    'Create-Owner'                 => 'Create Owner',
+    'Edit-Owner'                   => 'Edit Owner',
 
-    'Visitor-List' => 'Visitor List',
-    'Visitor-Year' => 'Create Visitor',
-    'Edit-Visitor' => 'Edit Visitor',
+    'Tenant-List'                  => 'Tenant List',
+    'Create-Tenant'                => 'Create Tenant',
+    'Edit-Tenant'                  => 'Edit Tenant',
 
-    'Email-List' => 'Email List',
-    'Create-Email' => 'Create Email',
-    'Edit-Email' => 'Edit Email',
+    'Year-List'                    => 'Year List',
+    'Create-Year'                  => 'Create Year',
+    'Edit-Year'                    => 'Edit Tenant',
 
-    'Owner-Utility-List' => 'Owner-Utility List',
-    'Create-Owner-Utility' => 'Create Owner-Utility',
-    'Edit-Owner-Utility' => 'Edit Owner-Utility',
+    'Visitor-List'                 => 'Visitor List',
+    'Visitor-Year'                 => 'Create Visitor',
+    'Edit-Visitor'                 => 'Edit Visitor',
 
-    'Management-Management-List' => 'Management-Management List',
+    'Email-List'                   => 'Email List',
+    'Create-Email'                 => 'Create Email',
+    'Edit-Email'                   => 'Edit Email',
+
+    'Owner-Utility-List'           => 'Owner-Utility List',
+    'Create-Owner-Utility'         => 'Create Owner-Utility',
+    'Edit-Owner-Utility'           => 'Edit Owner-Utility',
+
+    'Management-Management-List'   => 'Management-Management List',
     'Create-Management-Management' => 'Create Management-Management',
-    'Edit-Management-Management' => 'Edit Management-Management',
-    'List-Rent-Collection' => 'Edit Rent Collection',
+    'Edit-Management-Management'   => 'Edit Management-Management',
+    'List-Rent-Collection'         => 'Edit Rent Collection',
 ];
